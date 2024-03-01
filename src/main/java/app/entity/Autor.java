f62b154a916976937504b6515efef89ec309eb60
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-01 00:12:32 -0300

    Add Application.Properties

--- src/main/java/app/entity/Autor.java
+++ src/main/java/app/entity/Autor.java
@@ -1,5 +1,14 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,11 +16,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-
+@Entity
 public class Autor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@NotNull(message = "Campo Nome e CPF não pode ser nulo")
 	private String nome;
 	private String cpf;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("Autor")
 	private int idade;
 }
